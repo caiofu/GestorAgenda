641ad5e88d87206b2bded82c16dd48ca981ebbfd
Caio Furegati <caiofu@hotmail.com>
2023-08-31 13:54:18 -0300

    Feito o layout do componente estabelecimento

--- components/Estabelecimento/Estabelecimento.js
+++ components/Estabelecimento/Estabelecimento.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { TextInput } from 'react-native-paper';
-import {SafeAreaView, Text, } from "react-native";
+import {SafeAreaView, Text, Button, Image, View, StyleSheet, TouchableOpacity } from "react-native";
 import * as ImagePicker from 'expo-image-picker';
 
 //ESTILO
@@ -9,13 +9,41 @@ import styles from './StyleEstabelecimento';
 
 export default function Estabelecimento()
 {
+    const [image, setImage] = useState(null);
+    const selecionaImagem = async () => {
+       
+        let resultado = await ImagePicker.launchImageLibraryAsync({
+            //Definindo as opções do imagePicker
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4,3]
+        });
+      
+        if(resultado.canceled)
+        {
+            console.log('seleção cancelada'); //Posteriormente podemos retirar esse if era para testar e ter uma base do funcionamento
+        }
+        else if (resultado.assets)
+        {
+            setImage(resultado.assets[0].uri);
+        
+        }
+    }
    //FOI INSTALADO O IMAGE PICKER DO EXPO (FAZER TRATATIVAS PARA FUNCIONAR)
     return(
-        <SafeAreaView sltyle={styles.container}>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.boxLogo}>
+                {image != null ? <Image source={{ uri: image }} style={{ width: 200, height: 200, alignSelf:'center' }} /> :<Image source={require('../../assets/logo/logo-app.png')} style={{ width: 200, height: 200 }} />}
+                <TouchableOpacity style={styles.btnLogo}   onPress={selecionaImagem}>
+                    <Text style={styles.btnLogoText}>SELECIONAR LOGO</Text>
+                </TouchableOpacity>
+            </View>  
             <TextInput label="Nome do Estabelecimento"   theme={{colors:{primary: '#006699'}}} style={styles.inputFormulario}/>
             <TextInput label="CNPJ"   theme={{colors:{primary: '#006699'}}} style={styles.inputFormulario}/>
             <TextInput label="Ramo de atividade"   theme={{colors:{primary: '#006699'}}} style={styles.inputFormulario}/>
-           
+            <TouchableOpacity style={styles.btnSalvar}>
+                <Text style={styles.btnSalvarText}>SALVAR</Text>
+            </TouchableOpacity>
         </SafeAreaView>
     )
 }
--- components/Estabelecimento/StyleEstabelecimento.js
+++ components/Estabelecimento/StyleEstabelecimento.js
@@ -6,9 +6,52 @@ const styles = StyleSheet.create({
     {
       
     },
+
     inputFormulario:
     {
         marginLeft:10, marginRight:10, marginBottom:13, backgroundColor: 'rgba(112, 120, 147, 0.3)'
+    },
+
+    boxLogo:
+    {
+        borderWidth:1, 
+        width:"50%", 
+        marginLeft:10, 
+        marginBottom:10, 
+        borderRadius:8
+    },
+    btnLogo:
+    {
+        backgroundColor:'#006699',
+        borderRadius:5,
+        padding:5,
+        fontWeight:'bold',
+        color:'red'
+    },
+
+    btnLogoText:
+    {
+        alignSelf:'center', 
+        color:'#ffff',
+        fontWeight:'bold'
+    },
+    
+    btnSalvar:
+    {
+        backgroundColor:'#006699',
+        marginLeft:20,
+        marginRight:20,
+        padding:20,
+        borderRadius:5
+    },
+
+    btnSalvarText:
+    {
+        color:'#ffff',
+        fontWeight:'bold',
+        fontSize:20,
+        alignSelf:'center',
+
     }
 });
 
