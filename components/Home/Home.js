b819e77a9099ede1afc6b847a31881628b23f78e
caiofu <caiofu@hotmail.com>
2023-09-27 14:47:41 -0300

    Atualizando layote da home (tamanho do botao)

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -30,7 +30,7 @@ export default function Home({ navigation }) {
         <View style={styles.boxBotao}>
           <View style={styles.iconeBotao}>
             <FontAwesome5 style={styles.iconeBotao} name="store" size={34} color='#ffffff' />
-            <Text style={styles.txtBotao}>Negócio</Text>
+            <Text style={styles.txtBotao}>Estabelecimento</Text>
           </View>
         </View>
       </TouchableOpacity>
