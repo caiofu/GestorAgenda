842c8b0b13a99a94feed1081429854e932826722
caiofu <caiofu@hotmail.com>
2023-09-29 00:48:25 -0300

    NovoServiço botões

--- components/Servicos/NovoServico.js
+++ components/Servicos/NovoServico.js
@@ -1,10 +1,130 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import { Button } from "react-native-paper";
+import { FontAwesome5, FontAwesome } from '@expo/vector-icons';
+import styles from "./StyleServicos";
+import { useEffect, useState } from "react";
+import { SelectList, MultipleSelectList } from "react-native-dropdown-select-list";
+
+//COMPONENTES CRIADOS
+import { ConsultaRamoAtividade } from "../SQLiteManager/SQLRamoAtividade";
+
+//CONTEXT
+import { useAppState } from "../Contexts/AppStateContext";
 
 export default function NovoServico()
 {
+     //COR DO TEMA
+     const {tema} = useAppState();
+     const [corTema, setCorTema] = useState('#006699');
+
+     //RATO DE ATIVIDADE
+     const [ramoAtividadeSelecionado, setRamoAtividadeSelecionado]          = useState(""); //Usado para seleção da lista
+     const [idRamoAtividade, setIdRamoAtividade]                            = useState(null);
+     const [ramoAtividade, setRamoAtividade]                     = useState(null);
+     const [listaRamoAtividade, setListaRamoAtividade]                      = useState([]);
+
+     //BOTOES
+     const [btnImportar, setBtnImportar] = useState(false);
+     const [btnNovo, setBtnNovo]            = useState(false);
+      useEffect(()=>{
+   
+       tema === 'light' ? setCorTema('#006699') : setCorTema(DarkTheme.colors.text);
+         },[tema])
+
+
+    useEffect(() =>{
+        if(btnImportar)
+        {
+            console.log('trazendo a lista')
+            ConsultaRamoAtividade((ramoAtividades) => {
+                const retorno = ramoAtividades.map((atividade) => ({
+                    key: atividade.idRamoAtividade.toString(),
+                    value: atividade.nomeAtividade,
+                }));
+                    //console.log(retorno);
+                    setListaRamoAtividade(retorno);
+                  
+                });
+        }
+
+    }, [btnImportar])
+
+    //UseEffect exlcuiso para o selectlis ramo de atividade
+    useEffect(() => {
+        // Obtendo chave correspondente ao valor do ramo de atividade
+        const chaveSelecionada = listaRamoAtividade.find(item => item.value === ramoAtividadeSelecionado)?.key;
+        //console.log("Chave (key) correspondente ao valor de ramo de atividade:", chaveSelecionada);
+        setIdRamoAtividade(chaveSelecionada);
+      }, [ramoAtividadeSelecionado, listaRamoAtividade]);
+
+    function ImportarServico()
+    {
+        setBtnImportar(true);
+    }
+    
     return(
-        <View>
-            <Text>Novo serviço</Text>
-        </View>
+        <SafeAreaView>
+
+            <View style={{display: 'flex', flexDirection: 'column', flexGrow:1, gap:20, margin:10}} >
+             {btnImportar ? 
+             (  
+                <>
+                    <SelectList
+                        placeholder="Selecione o ramo de atividade"
+                        searchPlaceholder=""
+
+                        fontFamily="Rubik_400Regular"
+                        boxStyles={styles.inputFormularioSelect}
+                        dropdownStyles={{ alignSelf: 'center', width: '89%' }}
+                        setSelected={(val) => { setRamoAtividade(val); } }
+                        data={listaRamoAtividade}
+                        dropdownTextStyles={{ color: corTema }}
+                        save="value"
+                        arrowicon={<FontAwesome5 name="chevron-down" size={17} color={corTema} />}
+                        searchicon={<FontAwesome5 name="search" size={17} color={corTema} />}
+                        closeicon={<FontAwesome name="close" size={24} color={corTema} />}
+                        // defaultOption={{key:idRamoAtividade,value:ramoAtividade}}
+                        inputStyles={{ color: corTema }} />
+
+                        <MultipleSelectList
+                            placeholder="Nenhum ramo selecionado"
+                        />
+
+                       
+                        
+                        <TouchableOpacity style={styles.btnDesabilitado} disabled={true}>
+                            <View style={[styles.btnContainer, {alignSelf:'center'}]}>
+                                <Text style={styles.btnText}>SALVAR</Text>
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.btnCancelar} disabled={true}>
+                            <View style={[styles.btnContainer, {alignSelf:'center'}]}>
+                                <Text style={styles.btnText}>CANCELAR</Text>
+                            </View>
+                        </TouchableOpacity>
+                </>
+             ) : (
+           
+                <>
+                    <TouchableOpacity style={styles.btn} onPress={ImportarServico}>
+                            <View style={styles.btnContainer}>
+                                <FontAwesome5 name="file-import" size={28} color="#fff" />
+                                <Text style={styles.btnText}>importar serviço</Text>
+                            </View>
+                        </TouchableOpacity>
+                        
+                        <TouchableOpacity style={styles.btn}>
+                                <View style={styles.btnContainer}>
+                                    <FontAwesome name="plus" size={28} color="#fff" />
+                                    <Text style={styles.btnText}>Criar novo</Text>
+                                </View>
+                        </TouchableOpacity>
+
+                       
+                </>
+           )}
+            </View>
+                        
+        </SafeAreaView>
     )
 }
--- components/Servicos/StyleServicos.js
+++ components/Servicos/StyleServicos.js
@@ -8,53 +8,14 @@ const styles = StyleSheet.create({
         justifyContent:'center',
     },
 
-    inputFormulario:
-    {
-        marginLeft:15, 
-        marginRight:15, 
-        marginBottom:20, 
-        backgroundColor: 'rgba(112, 120, 147, 0.3)',
-    },
+  
+  
+    btnContainer: {
+        flexDirection: "row", // Alinha os filhos (ícone e texto) lado a lado
+        alignItems: "center", // Alinha verticalmente ao centro
+      },
 
-    boxLogo:
-    {
-        borderWidth:1, 
-        width:"50%", 
-        marginBottom:15, 
-        marginTop:15,
-        paddingRight:6,
-        paddingLeft:6,
-        borderRadius:8,
-        alignSelf:'center',
-    },
-    btnLogo:
-    {
-        backgroundColor:'#006699',
-        borderRadius:5,
-        padding:8,
-        fontWeight:'bold',
-        color:'red',
-        marginBottom:2,
-        marginTop:2
-    },
-
-    btnLogoText:
-    {
-        alignSelf:'center', 
-        color:'#ffff',
-        fontFamily:'Rubik_700Bold'
-        
-    },
-
-    imgLogo:
-    {
-        width: 190,
-        height: 190,
-        alignSelf: 'center',
-        margin:2
-    },
-    
-    btnSalvar:
+    btn:
     {
         backgroundColor:'#006699',
         marginLeft:20,
@@ -62,7 +23,17 @@ const styles = StyleSheet.create({
         padding:20,
         borderRadius:5
     },
-    btnSalvarDesabilitado:
+
+    btnCancelar:
+    {
+        backgroundColor:'#fd1c1c',
+        marginLeft:20,
+        marginRight:20,
+        padding:20,
+        borderRadius:5
+    },
+    
+    btnDesabilitado:
     {
         backgroundColor:'#9e9e9e',
         marginLeft:20,
@@ -70,10 +41,10 @@ const styles = StyleSheet.create({
         padding:20,
         borderRadius:5
     },
-    btnSalvarText:
+    btnText:
     {
         color:'#ffff',
-      
+        marginLeft:10,
         fontSize:20,
         alignSelf:'center',
         fontFamily:'Rubik_700Bold'
@@ -86,59 +57,12 @@ const styles = StyleSheet.create({
         fontFamily:'Rubik_300Light'
     },
 
-    txtAnimacao:
-    {
-        alignSelf:'center',
-        color:'#006699',
-        fontFamily:'Rubik_700Bold',
-        fontSize:22,
-        marginTop:6
-    },
+  
 
-    contornoModal:
-    {
-        flex: 1, 
-        justifyContent: 'center', 
-        alignItems: 'center',
-        backgroundColor: 'rgba(128, 128, 128, 0.5)'
-    },
-    janelaModal:
-    {
-        margin: 20,
-        backgroundColor: 'white',
-        borderRadius: 20,
-        padding: 35,
-        alignItems: 'center',
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
-    },
-    txtModal:
-    {
-        fontFamily:'Rubik_700Bold',
-        color:'#006699',
-        fontSize:18
-    },
-    btnModal:
-    {
-        marginTop:28,
-        
-        backgroundColor:"#006699",
-        padding:8,
-        borderRadius:9,
-
-    },
-    txtBtnModal:
-    {
-        color:'#ffff',
-        fontFamily:'Rubik_700Bold',
-
-    }
+    
+    
+   
+  
 
 });
 
