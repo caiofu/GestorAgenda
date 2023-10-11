05f922e87e6cd0788b589a214499492afb88fb28
caiofu <caiofu@hotmail.com>
2023-10-10 22:37:27 -0300

    DapaPicker funcionando

--- components/Agendamento/Agendamento.js
+++ components/Agendamento/Agendamento.js
@@ -5,17 +5,39 @@ import { useState } from "react";
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 
+
 export default function Agendamento()
 {
     const MORE_ICON = Platform.OS === 'ios' ? 'dots-horizontal' : 'dots-vertical';
     const [date, setDate] = useState(new Date());
-    const [dataAtual, setDataAtual] = useState({dia: date.getDate(), mes: date.getMonth()+1, ano: date.getFullYear()});
-    return(
-        <View style={{flex:1, padding:10, flexDirection:'column'}}>
-            <TouchableOpacity style={{borderWidth:1, flexDirection:'row'}}>
-                <FontAwesome  name="calendar" size={24}  />
-                <Text>{dataAtual.dia+'/'+dataAtual.mes+'/'+dataAtual.ano}</Text>
+    const [dataFormatada, setDataFormatada] = useState({dia: date.getDate(), mes: date.getMonth()+1, ano: date.getFullYear()});
+
+    const [abriDataPicker, setAbrirDataPicker] = useState(false);
+    const [mode, setMode]    = useState('date');
+
+    
+    const onChangeDataPicker = (event, dataSelecionada) => {
+        setDate(dataSelecionada);
+        setDataFormatada({dia: date.getDate(), mes: date.getMonth()+1, ano: date.getFullYear()})
+        setAbrirDataPicker(false); // Fecha o DatePicker quando a data é selecionada
+    }
+
+    const showDatePicker = () => {
+        setMode('date'); // Define o modo como data
+        setAbrirDataPicker(true);
+    }
+
+ console.log(date)
+    return (
+        <View style={{flex: 1, padding: 10, flexDirection: 'column'}}>
+            <TouchableOpacity style={{borderWidth: 1, flexDirection: 'row', padding: 4}} onPress={showDatePicker}>
+                <FontAwesome name="calendar" size={24} />
+                <Text>{dataFormatada.dia + '/' + dataFormatada.mes + '/' + dataFormatada.ano}</Text>
             </TouchableOpacity>
+            {abriDataPicker ? (
+                <DateTimePicker value={date} mode={mode} onChange={onChangeDataPicker} />
+            ) : null}
+          
         </View>
     );
 }
